f1772c9c87cf37bea310da9d491911c313e28bc9
hsdyyd <yidong06@sina.com>
2019-01-15 21:34:46 +0800

    工程完成

--- src/main/java/com/train/springboot/springboot/Springboot02Config02Application.java
+++ src/main/java/com/train/springboot/springboot/Springboot02Config02Application.java
@@ -8,6 +8,7 @@ public class Springboot02Config02Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Springboot02Config02Application.class, args);
+
     }
 
 }
